86cf2354de7494d5c1193ef5d7b51efe2b2f6c8c
Karen Cristine Gonçalves dos Santos <cris.kgs@gmail.com>
2019-12-16 11:37:40 -0500

    Update CustomSelection.R

--- R/CustomSelection.R
+++ R/CustomSelection.R
@@ -47,7 +47,14 @@
 #'
 #' @section ath_featureLength dataset:
 #' Length of Arabidopsis thaliana genes (TAIR10) obtained with the following code:
-#'
+#' \code{
+#' library(biomaRt) ath <- useMart('plants_mart', host = "plants.ensembl.org", dataset = "athaliana_eg_gene") 
+#' 
+#' gene_start_end = getBM(attributes = c('ensembl_gene_id', 'start_position', 'end_position'), mart = ath) 
+#' 
+#' featureLength <- gene_start_end$end_position - gene_start_end$start_position 
+#' names(featureLength) <- gene_start_end$ensembl_gene_id
+#'}
 #' @docType package
 #' @name CustomSelection
 NULL
