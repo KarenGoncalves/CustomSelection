8e8dbb380ee6230775d467194b974b65e868b2b2
Karen Cristine Gonçalves dos Santos <cris.kgs@gmail.com>
2019-12-16 13:42:53 -0500

    Add files via upload

--- R/ath_featureLength.R
+++ R/ath_featureLength.R
@@ -1,16 +1,17 @@
 #' Example - vector with gene length
 #'
 #' Length of Arabidopsis thaliana genes (TAIR10) obtained with the following code:
-#' 
-#' \code{library(biomaRt)
-#' ath <- useMart('plants_mart', host = "plants.ensembl.org", dataset = "athaliana_eg_gene")
-#' 
-#' gene_start_end = getBM(attributes = c('ensembl_gene_id', 'start_position', 'end_position'), mart = ath)
-#' 
-#' featureLength <- gene_start_end$end_position - gene_start_end$start_position
-#' 
-#' names(featureLength) <- gene_start_end$ensembl_gene_id}
-#' 
+#'
+#' \code{library(biomaRt)}
+#'
+#' \code{ath <- useMart('plants_mart', host = "plants.ensembl.org", dataset = "athaliana_eg_gene")}
+#'
+#' \code{gene_start_end = getBM(attributes = c('ensembl_gene_id', 'start_position', 'end_position'), mart = ath)}
+#'
+#' \code{featureLength <- gene_start_end$end_position - gene_start_end$start_position}
+#'
+#' \code{names(featureLength) <- gene_start_end$ensembl_gene_id}
+#'
 #' @format Name vector: length (values) of genes (names) of Arabidopsis thaliana
 #' @source \url{plants.ensembl.org}
 "ath_featureLength"
